d1ea61c6c7e34004b31abb7057bca9556bf3688a
daoraCode <severin.mboukou@gmail.com>
2020-12-06 15:07:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Recherches d'informations par états</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
