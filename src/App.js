39f431e0b779c4ce74d7970f1e8a50a17b7fc981
daoraCode <severin.mboukou@gmail.com>
2020-12-06 19:23:22 +0100

    Importation de la librairie axios, création de la méthode hook , création du constructeur

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+
+// Importation de librairie axios afin d'effectuer des requêtes HTTP dans l'application ReactJS
+import axios from 'axios';
+
 import './App.css';
 
+// lien API : https://api.schooldigger.com/v1/schools
+
 class App extends Component {
-  render() {
+
+  /* Méthode componentDidMount(), hook qui va permettre */
+  componentDidMount() {
+    axios.get('https://api.schooldigger.com/v1/schools');
+  }
+
+  constructor(props) {
+  /* - Creation du constructeur, 
+     - Appel de la classe parente, (superclasse) : keyword `super`*/
+    super(props);
+
+    this.state = {value: ''};
+
+    /* Rappel des méthodes  */
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  
+
+
+/* Structure du code JSX, préparations */
+render() {
     return (
-      <div className="App">
-        <h1>Recherches d'informations par états</h1>
+      // Lignes de code JSX
+      <div className="mainTitle">
+        <h1>Recherches d'Informations par états</h1>
+        <h1 className="subTitle">getAllSchools</h1>    
       </div>
     );
   }
