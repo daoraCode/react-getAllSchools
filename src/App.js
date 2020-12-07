1d810ae6599b62facac44ec2d4281b77982b9f63
daoraCode <severin.mboukou@gmail.com>
2020-12-07 06:31:08 +0100

    la console affiche bien les valeurs insérées dans les champs de texte, mieux vaut rester avec les méthodes fléchées - arrows methods - de cette manière les events comme preventDefault fonctionneront correctement

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 import React, { Component } from 'react'
 
-// Importation de librairie axios afin d'effectuer des requêtes HTTP dans l'application ReactJS
-//import axios from 'axios';
+/* Importation de librairie axios afin d'effectuer des requêtes HTTP dans l'application ReactJS */
+// import axios from 'axios';
 
 import './App.css';
 
 // lien API : https://api.schooldigger.com/v1/schools
 
-class App extends Component {
+class App extends Component { 
+
+
+    /* - Informations/données (donnéees API de getAllSchools) 
+    à afficher quand le Composant utilise le render() est lancé */ 
    constructor(props) {
      super(props)
-
-
-
-     this.state = {
+      this.state = {
       numberOfPages: '',
       numberOfSchools: '',
       schoolList: '' 
      }
    }
 
-   changeHandler = (e) => {
+   // méthode événementielle consacrée aux inputs
+   changeHandler = e => {
       this.setState({ [e.target.name]: e.target.value })
    }
 
+   // méthode événementielle consacrée aux envois
    submitHandler = e => {
-     e.preventDefault()
-     console.log(this.state)
+     e.preventDefault() // pour éviter un rafraichissement de la page
+     console.log(this.state) // App.js:32 {numberOfPages: "", numberOfSchools: "", schoolList: ""}
    }
-
-
   
 
 
-/* Structure du code JSX, préparations */
+  /* Méthode reponsable de l'interface, à ressemblera l'UI construite */
 render() {
-  // destructuration des valeurs attribuées ci-dessus
+  /* Structure du code JSX, préparations */
+
+  // - destructuration des valeurs attribuées API définies dans le constructeurs
+
+  /* En créant une déstructuration : toute mes valeurs seront 
+  décomposées et équivaudront d'une certaine manière à une seule et même variable `this.state`, méthode permettant donc de raccourcir le code  */
+
   const { numberOfPages, numberOfSchools, schoolList } = this.state; 
+ // - ! placer defaultValue au lieu de value, auquel cas il y aura des erreurs ------------------ DESTRUCTURING
+ // - le defautValue ne causera pas la mise à jour de la valeur initiale du DOM
+
+
     return (
       // Lignes de code JSX
       <div className="mainTitle">
+
+
+
 
         <form onSubmit={this.submitHandler}>
           
@@ -47,7 +61,7 @@ render() {
 
           <input type="text" 
           name="numberOfPages" 
-          value={numberOfPages} 
+          defaultValue={numberOfPages} 
           onChange={this.numberOfPages} />
 
           </div>
@@ -57,7 +71,7 @@ render() {
 
           <input type="text" 
           name="numberOfSchools" 
-          value={numberOfSchools} 
+          defaultValue={numberOfSchools} 
           onChange={this.numberOfSchools} />
 
           </div>
@@ -67,7 +81,7 @@ render() {
           <input 
           type="text" 
           name="schoolList" 
-          value={schoolList} 
+          defaultValue={schoolList} 
           onChange={this.schoolList} />
 
           </div>
