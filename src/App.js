4fdfc9e46fed34206a9139d78859fd50bfa308cf
daoraCode <severin.mboukou@gmail.com>
2020-12-07 05:22:54 +0100

    Création de formulaire, affectation et destructuration, création de méthodes submit et handler

--- src/App.js
+++ src/App.js
@@ -1,43 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 // Importation de librairie axios afin d'effectuer des requêtes HTTP dans l'application ReactJS
-import axios from 'axios';
+//import axios from 'axios';
 
 import './App.css';
 
 // lien API : https://api.schooldigger.com/v1/schools
 
 class App extends Component {
+   constructor(props) {
+     super(props)
 
-  /* Méthode componentDidMount(), hook qui va permettre */
-  componentDidMount() {
-    axios.get('https://api.schooldigger.com/v1/schools');
-  }
 
-  constructor(props) {
-  /* - Creation du constructeur, 
-     - Appel de la classe parente, (superclasse) : keyword `super`*/
-    super(props);
 
-    this.state = {value: ''};
+     this.state = {
+      numberOfPages: '',
+      numberOfSchools: '',
+      schoolList: '' 
+     }
+   }
 
-    /* Rappel des méthodes  */
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-  }
+   changeHandler = (e) => {
+      this.setState({ [e.target.name]: e.target.value })
+   }
+
+   submitHandler = e => {
+     e.preventDefault()
+     console.log(this.state)
+   }
+
 
   
 
 
 /* Structure du code JSX, préparations */
 render() {
+  // destructuration des valeurs attribuées ci-dessus
+  const { numberOfPages, numberOfSchools, schoolList } = this.state; 
     return (
       // Lignes de code JSX
       <div className="mainTitle">
-        <h1>Recherches d'Informations par états</h1>
-        <h1 className="subTitle">getAllSchools</h1>    
+
+        <form onSubmit={this.submitHandler}>
+          
+          <div className="inputData">
+
+          <input type="text" 
+          name="numberOfPages" 
+          value={numberOfPages} 
+          onChange={this.numberOfPages} />
+
+          </div>
+
+
+          <div className="inputData">
+
+          <input type="text" 
+          name="numberOfSchools" 
+          value={numberOfSchools} 
+          onChange={this.numberOfSchools} />
+
+          </div>
+          
+          <div className="inputData">
+
+          <input 
+          type="text" 
+          name="schoolList" 
+          value={schoolList} 
+          onChange={this.schoolList} />
+
+          </div>
+
+          <button type="submit">Envoyer</button>
+        </form>    
       </div>
-    );
+    )
   }
 }
 
